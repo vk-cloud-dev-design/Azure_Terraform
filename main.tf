b65870dde816866d4e9e59ceccc67ad4204abee7
vikas_kumar <vk98358994@gmail.com>
2026-08-06 18:09:24 +0530

    added RG3

--- main.tf
+++ main.tf
@@ -21,3 +21,9 @@ resource "azurerm_resource_group" "vikas_RG2" {
   name = "vikas_RG2"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "vikas_RG3" {
+
+  name = "vikas_RG3"
+  location = "West Europe"
+}
